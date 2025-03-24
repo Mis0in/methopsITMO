dd742b9a0f365157eb570811a109df4f663f8d31
Mikhail Boin <138712459+Mis0in@users.noreply.github.com>
2025-03-24 16:37:32 +0300

    last working version of code

--- gd.py
+++ gd.py
@@ -8,16 +8,13 @@ def get_dx(dim, index, eps=1e-8):
     dx[index] = eps
     return dx
     
-def forward_difference(f, x, dx, eps=1e-8):
+def approximate_deriv(f, x, dx, eps=1e-8):
     return (f(x + dx) - f(x)) / eps
 
-def backward_difference(f, x, dx, eps=1e-8):
-    return (f(x - dx) - f(x)) / eps 
+def symmetric_deriv(f, x, dx, eps=1e-8):
+    return (f(x + dx) - f(x - dx)) / (2 * eps)
 
-def central_difference(f, x, dx, eps=1e-8):
-    return (f(x + dx) - f(x - dx)) / (2 * eps) 
-
-def approximate_gradient(f, x, eps=1e-8, dif = central_difference):
+def approximate_gradient(f, x, eps=1e-8, dif = symmetric_deriv):
     grad = np.zeros_like(x)
     for i in range(len(x)):
         
@@ -27,10 +24,10 @@ def approximate_gradient(f, x, eps=1e-8, dif = central_difference):
 
 # ---gradient descent algorithm---
 
-def id_one(*args):
+def identity_one(*args):
     return 1
 
-def gradient_descent(f, start, lrate = id_one, search = id_one, iter_limit=1000, e=1e-6):
+def gradient_descent(f, start, lrate = identity_one, search = identity_one, iter_limit=1000, e=1e-6):
     x = start.copy()
     k = 0
 
@@ -56,8 +53,8 @@ def fixed_step(alpha):
 def h0(k):
     return 1 / math.sqrt(k + 1)
 
-def exponential_decay(alpha):
-    return lambda k: h0(k) * np.exp(-alpha * k)
+def exponential_decay(lamb):
+    return lambda k: h0(k) * np.exp(-lamb* k)
 
 def polynomial_decay(alpha, beta):
     return lambda k: h0(k) * (beta * k + 1)**(-alpha)
@@ -70,37 +67,33 @@ def armijo_rule(f, x, direction, alpha=0.5, q=0.5, c=0.5):
             return alpha
         else:
             alpha *= q
- 
-def wulfs_rule(f,x,direction, alpha=0.5, q=0.5, c1=0.5, c2=0.5):
-     while True:
-        if f(x + alpha * direction) <= f(x) + c1 * alpha * np.linalg.norm(direction) and  
-            return alpha
-        else:
-            alpha *= q 
 
 # ---testing, examples---
 
 def f(x):
-    return x[0]**2 * x[1]**2
+    return x[0]**2 + x[1]**2
 
 start = np.array([-42.0, 31.0])
 
-def gd_with_lrate(f, start):
-    return lambda rate : gradient_descent(f, start, lrate = rate) 
+result_fixed = gradient_descent(
+    f, start,
+    lrate=fixed_step(0.05),
+)
 
-def gd_with_search(f, start):
-    return lambda search : gradient_descent(f, start, search=search)
+result_exp_decay = gradient_descent(
+    f, start,
+    lrate=exponential_decay(0.01),  
+)
 
-gdrate = gd_with_lrate(f,start)
-gdsearch = gd_with_search(f, start)
+result_pol_decay = gradient_descent(
+    f, start,
+    lrate=polynomial_decay(0.5, 1),
+)
 
-result_fixed = gdrate(fixed_step(0.05))
-
-result_exp_decay = gdrate(exponential_decay(0.01))
-
-result_pol_decay = gdrate(polynomial_decay(0.5, 1))
-
-result_armijo = gdsearch(lambda f, x, direction: armijo_rule(f, x, direction))
+result_armijo = gradient_descent(
+    f, start,
+    search=lambda f, x, direction: armijo_rule(f, x, direction)
+)
 
 print("fixed step:", result_fixed)
 print("exponential decay:", result_exp_decay)
